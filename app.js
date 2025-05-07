52c823bb3e9c0889730666f65237c3c4dfea70b5
hugo-gaudre <hugo.gaudre@icloud.com>
2025-05-07 10:57:11 +0200

    Ajout d'un limiteur de requete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const userRouter = require('./route/user.route.js');
 const authRouter = require('./route/auth.route.js');
@@ -7,6 +8,14 @@ const roleRouter = require('./route/role.route.js');
 const {connect} = require('./framework/connection.js');
 const sync = require('./framework/sync.js');
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'Vous avez fait trop de requetes, il faudra ressayer plus tard'
+});
+
 const database = async () => {
     await connect();
     await sync();
@@ -14,6 +23,7 @@ const database = async () => {
 
 database();
 
+app.use(limiter);
 app.use(express.json());
 
 app.use('/user',userRouter);
